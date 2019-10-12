bd4890c8375f820defa7ad0bfcb73109352a9bab
Paulck <paul.netp@gmail.com>
2019-10-12 14:42:51 -0400

    Changed Site Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Esthers Arise : Overcome And Live Out Gods Promises',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'An Army of Women Arising',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
