370be465a9cbedf285ee28520449097c72d3d89d
Paul <paul@brightmarkmedia.com>
2019-10-14 09:54:55 -0400

    Footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,6 @@ import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -22,7 +20,6 @@ const Footer = class extends React.Component {
           <div className="container">
             <div className="columns">
               <div className="column is-4">
-                <section className="menu">
                   <ul className="menu-list">
                     <li>
                       <Link to="/" className="navbar-item">
@@ -35,10 +32,8 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                   </ul>
-                </section>
               </div>
               <div className="column is-4">
-                <section>
                   <ul className="menu-list">
                     <li>
                       <Link className="navbar-item" to="/contact">
@@ -56,35 +51,19 @@ const Footer = class extends React.Component {
                       </a>
                     </li>
                   </ul>
-                </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/esthersarise/">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://www.instagram.com/esthers_arise/">
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
