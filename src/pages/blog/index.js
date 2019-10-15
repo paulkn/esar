e68b29d11fb78a728d145603f84a8d82303ca64d
Paul <paul@brightmarkmedia.com>
2019-10-15 15:58:03 -0400

    Update Blog Title Css

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,11 +16,13 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
+              boxShadow:
+                'rgba(43, 31, 91, .64) 0.5rem 0px 0px, rgba(43, 31, 91, .64) -0.5rem 0px 0px',
+              backgroundColor: 'rgba(43, 31, 91, .64)',
               color: 'white',
-              padding: '1rem',
-            }}
+              lineHeight: '1',
+              padding: '0.25em',
+                  }}
           >
             Latest Stories
           </h1>
