d37954b225ea79b1c14a53fb47a195b2afd96460
Paul <paul.netp@gmail.com>
2020-11-05 14:17:00 -0500

    Base CMS Vanilla Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Esthers Arise" style={{ width: '88px' }} />
+              <img src={logo} alt="Brand Logo Alt Text" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
