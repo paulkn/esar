085ad0b43ae6d9cb062b20aa89da0457ebe2de26
Paul <paul.netp@gmail.com>
2021-02-15 18:38:19 -0500

    Blog page image

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,7 +10,7 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0 blog"
           style={{
-            backgroundImage: `url('/img/b-white-flowers.png')`,
+            backgroundImage: `url('/img/blog.jpg')`,
           }}
         >
           <h1
